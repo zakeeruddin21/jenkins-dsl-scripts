58199eed483d94baf34f08e1ef3caf693da55fb1
Zakeeruddin <zakeeruddin_mohammed@gap.com>
2017-02-20 19:13:42 -0800

    Inital commit

---
+++ exampleJob.groovy
@@ -0,0 +1,11 @@
+job('exampleJob') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
